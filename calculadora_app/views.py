eb479f50ba51c7ba473eebcf72df5467a92c5060
gabi0327 <164135565+gabi0327@users.noreply.github.com>
2024-11-28 21:33:58 -0500

    Add files via upload

---
+++ calculadora_app/views.py
@@ -0,0 +1,63 @@
+# Create your views here.
+from django.shortcuts import render
+
+
+
+
+def calculadora_para_invitados(request):
+    return render(request,'calculadora_para_invitados.html')
+
+
+def calculadora(request):
+    return render(request,'calculadora.html')
+
+
+
+def mostrar_capitulo(request):
+    return render(request,'mostrar_capitulo.html')
+
+
+def insertar_capitulo(request):
+    return render(request,'insertar_capitulo.html')
+
+
+
+
+def mostrar_peliculas(request):
+    return render(request,'mostrar_pelicula.html')
+
+
+def insertar_pelicula(request):
+    return render(request,'insertar_pelicula.html')
+
+
+
+
+
+def mostrar_juego(request):
+    return render(request,'mostrar_juego.html')
+
+
+def insertar_juego(request):
+    return render(request,'insertar_juego.html')
+
+
+
+
+def login(request):
+    return render(request,'login.html')
+
+
+
+def mostrar_usuario(request):
+    return render(request,'mostrar_usuario.html')
+
+
+def insertar_usuario(request):
+    return render(request,'insertar_usuario.html')
+
+
+def inicio(request):
+    return render(request,'inicio.html')
+
+
